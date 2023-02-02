9092ee86769ca3f273659011703f3763de09827d
lucianofcm <lucweb@gmail.com>
2023-02-02 00:13:20 -0300

    make it better

--- src/main/java/br/com/lucapp/controller/DriveController.java
+++ src/main/java/br/com/lucapp/controller/DriveController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -75,11 +74,11 @@ public class DriveController {
 	}
 
 	@PostMapping(value="/upload")
-	public String handleFileUpload(@RequestPart MultipartFile file,
+	public String handleFileUpload(@RequestParam(value="file") MultipartFile file,
 			@RequestParam("pastaDestino") String pastaDestino) throws IllegalStateException, IOException {
 
-		System.out.println("ENTROU.......");
-		//fileDownloadUtil.uploadArquivo(file,pastaDestino);
+		
+		fileDownloadUtil.uploadArquivo(file,pastaDestino);
 
 		return "redirect:/";
 	}
