c4696fcd1b77c0fb9db90a3b44398d5e1cad5a2d
lucianofcm <lucweb@gmail.com>
2023-02-02 01:28:09 -0300

    make it better

--- src/main/java/br/com/lucapp/LucappApplication.java
+++ src/main/java/br/com/lucapp/LucappApplication.java
@@ -1,9 +1,13 @@
 package br.com.lucapp;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -15,5 +19,17 @@ public class LucappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LucappApplication.class, args);
 	}
+	
+	 @Bean
+	 public MultipartConfigElement multipartConfigElement() {
+	     return new MultipartConfigElement("");
+	 }
+
+	 @Bean
+	 public MultipartResolver multipartResolver() {
+	     org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver = new org.springframework.web.multipart.commons.CommonsMultipartResolver();
+	     multipartResolver.setMaxUploadSize(1000000);
+	     return multipartResolver;
+	 }
 
 }
